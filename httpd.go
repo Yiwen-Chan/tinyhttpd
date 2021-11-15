6e6924d2bd7992fa4fbcecaf0717101dc61b29f9
Kanri <yiwen_chan@foxmail.com>
2021-11-15 22:22:51 +0800

    🎉 Update

--- httpd.go
+++ httpd.go
@@ -2,11 +2,15 @@
 package main
 
 import (
+	"bufio"
+	"errors"
+	"fmt"
 	"net"
+	"strings"
 )
 
 func main() {
-	listener, err := StartUp(":8080")
+	listener, err := StartUp("127.0.0.1:8080")
 	if err != nil {
 		panic(err)
 	}
@@ -15,14 +19,27 @@ func main() {
 		if err != nil {
 			continue
 		}
-		conn.Write([]byte("xxx"))
+		AcceptRequest(conn)
 		conn.Close()
 	}
 
 }
 
-func AcceptRequest(client int) {
-
+func AcceptRequest(client net.Conn) error {
+	var method string
+	br := bufio.NewReader(client)
+	b, _, err := br.ReadLine()
+	if err != nil {
+		return err
+	}
+	buf := string(b)
+	i := strings.Index(buf, " ")
+	if i == -1 {
+		return errors.New("Unknown method")
+	}
+	method = buf[:i]
+	fmt.Printf("v%sv", method)
+	return nil
 }
 
 func StartUp(port string) (net.Listener, error) {
