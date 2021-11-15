e9eb81df1e0f78e3814568dcf5b9c1cd6b1b89ea
Kanri <yiwen_chan@foxmail.com>
2021-11-15 23:59:16 +0800

    🎉 Update

--- httpd.go
+++ httpd.go
@@ -3,43 +3,81 @@ package main
 
 import (
 	"bufio"
-	"errors"
 	"fmt"
 	"net"
 	"strings"
 )
 
+const SERVER_STRING = "Server: jdbhttpd/0.1.0\r\n"
+
 func main() {
-	listener, err := StartUp("127.0.0.1:8080")
+	server, err := StartUp("127.0.0.1:8080")
 	if err != nil {
 		panic(err)
 	}
+	defer server.Close()
 	for {
-		conn, err := listener.Accept()
+		client, err := server.Accept()
 		if err != nil {
 			continue
 		}
-		AcceptRequest(conn)
-		conn.Close()
+		AcceptRequest(client)
+		client.Close()
 	}
-
 }
 
 func AcceptRequest(client net.Conn) error {
-	var method string
+	var (
+		method string
+	)
 	br := bufio.NewReader(client)
 	b, _, err := br.ReadLine()
 	if err != nil {
 		return err
 	}
-	buf := string(b)
-	i := strings.Index(buf, " ")
-	if i == -1 {
-		return errors.New("Unknown method")
+	var buf = string(b)
+	if i := strings.Index(buf, " "); i != -1 {
+		method = buf[:i]
+	} else {
+		UnimpleMented(client)
+		return nil
 	}
-	method = buf[:i]
-	fmt.Printf("v%sv", method)
+	if method != "GET" && method != "POST" {
+		UnimpleMented(client)
+		return nil
+	}
+	if method == "GET" {
+
+	}
+	fmt.Println(buf)
 	return nil
+}
+
+func UnimpleMented(client net.Conn) {
+	bw := bufio.NewWriterSize(client, 1024)
+	bw.WriteString("HTTP/1.0 501 Method Not Implemented\r\n")
+	bw.WriteString(SERVER_STRING)
+	bw.WriteString("Content-Type: text/html\r\n")
+	bw.WriteString("\r\n")
+	bw.WriteString("<HTML><HEAD><TITLE>Method Not Implemented\r\n")
+	bw.WriteString("</TITLE></HEAD>\r\n")
+	bw.WriteString("<BODY><P>HTTP request method not supported.\r\n")
+	bw.WriteString("</BODY></HTML>\r\n")
+	bw.Flush()
+}
+
+func NotFound(client net.Conn) {
+	bw := bufio.NewWriterSize(client, 1024)
+	bw.WriteString("HTTP/1.0 404 NOT FOUND\r\n")
+	bw.WriteString(SERVER_STRING)
+	bw.WriteString("Content-Type: text/html\r\n")
+	bw.WriteString("\r\n")
+	bw.WriteString("<HTML><TITLE>Not Found</TITLE>\r\n")
+	bw.WriteString("<BODY><P>The server could not fulfill\r\n")
+	bw.WriteString("your request because the resource specified\r\n")
+	bw.WriteString("is unavailable or nonexistent.\r\n")
+	bw.WriteString("</BODY></HTML>\r\n")
+	bw.Flush()
 }
 
 func StartUp(port string) (net.Listener, error) {
